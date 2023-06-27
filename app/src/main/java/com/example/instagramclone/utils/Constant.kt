bda91a2233c4400abd3bc261bf1b825b4afb9cd4
fuad <fuadhevpc@gmail.com>
2023-06-27 17:32:25 +0400

    Ignore google-service.json

--- app/src/main/java/com/example/instagramclone/utils/Constant.kt
+++ app/src/main/java/com/example/instagramclone/utils/Constant.kt
@@ -1,5 +1,5 @@
 package com.example.instagramclone.utils
 
 object Constant {
-    const val APP_ID="9b3b9701-9264-41ef-b08c-1c69f1fabfef" //onesignal app_id
+    const val APP_ID="Your OneSignal App_id" //onesignal app_id
 }
